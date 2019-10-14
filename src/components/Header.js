3c20f0d7e2690ec38b3478978c1c287e7f35992a
Stephenfre <freemanstephen37@gmail.com>
2019-10-13 22:09:20 -0700

    MVP Complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <div className="Nav-Container">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Character">Characters</NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
